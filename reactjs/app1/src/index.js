c51f85d07df03917232b9c7c8926aec50e93b48a
The EasyLearn Academy <theeasylearn@users.noreply.github.com>
2023-02-16 20:03:34 +0530

    bootstrap

--- reactjs/app1/src/index.js
+++ reactjs/app1/src/index.js
@@ -6,13 +6,28 @@ var num1 = 10
 var num2 = 3
 // we can use any mathematical expression in JSX
 var output = (<div className='container'>
-        <div className='row'>
-            <div className='col-12'>
-                <h1>Working with bootstrap</h1>
-            </div>
-            <div className='col-12'>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa porro nihil accusantium sunt iure, doloremque accusamus quod! Exercitationem dolores, beatae ea explicabo eius repudiandae necessitatibus quidem temporibus voluptatibus, nesciunt cumque?</p>
+    <div className='row'>
+        <div className='col-6 offset-3'>
+            <div className='card shadow'>
+                <div className='card-header text-bg-primary'>
+                    <h3>Login</h3>
+                </div>
+                <div className='card-body'>
+                    <form>
+                        <div className='mb-3'>
+                            <input type='email' className='form-control' placeholder='Email' />
+                        </div>
+                        <div className='mb-3'>
+                            <input type='password' className='form-control'
+                            placeholder='Password' />
+                        </div>
+                        <div className='mb-3 text-end'>
+                            <input type='button' className='btn btn-primary' value='Login' />&nbsp;<input type='reset' className='btn btn-secondary' value='clear all' />
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
-    </div>)
+    </div>
+</div>)
 root.render(output);
